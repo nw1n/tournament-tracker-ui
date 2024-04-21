55e213b303371d2fa4c4fe9d53c5dd1b26bcf1f7
nw1n <your@example.com>
2024-04-21 10:46:15 +0200

    refactor

--- lib/ServerApi.ts
+++ lib/ServerApi.ts
@@ -1,17 +1,17 @@
 export class ServerApi {
     static instance: ServerApi
 
-    serverUrl = 'http://localhost:5000'
-
-    async fetchAllServerData() {
-        const response = await fetch(`${this.serverUrl}`)
-        return await response.json()
-    }
+    public serverUrl = 'http://localhost:5000'
 
     static getInstance(): ServerApi {
         if (!ServerApi.instance) {
             ServerApi.instance = new ServerApi()
         }
         return ServerApi.instance
+    }
+
+    async fetchAllServerData() {
+        const response = await fetch(`${this.serverUrl}`)
+        return await response.json()
     }
 }
